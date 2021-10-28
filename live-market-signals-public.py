76fbfdda64589e1e58e579f89e0d1cf5e371bd38
acidsh0t <jtdejardin@gmail.com>
2021-10-28 22:27:42 +0800

    Update live-market-signals-public.py

--- live-market-signals-public.py
+++ live-market-signals-public.py
@@ -3,6 +3,9 @@
 '''
 Get live FOREX data
 Configured on Heroku to run on the hour every hour
+Works with IQ Option broker's leverage and settings
+Base leverage = 50
+Adjust SL proportionate to leverage difference
 
 Trading Strategy:
 - Only looks from last confirmed 1h candle
@@ -23,6 +26,9 @@ from tinydb.queries import Query
 #Data Source
 import yfinance as yf
 
+#graphing
+import plotly.graph_objs as go
+
 #gmail packages
 import smtplib
 from email.mime.text import MIMEText
@@ -30,11 +36,11 @@ from email.mime.text import MIMEText
 _time = datetime.utcnow()
 _time = str(_time + timedelta(hours = 8)) #Philippine timezone
 
-#quits if after :30
-half_hour = _time #converts time to string
-half_hour = int(half_hour[14:-10]) #only retains minutes
-if half_hour >= 30:
-    quit()
+hours = int(_time[11:-13]) #only retains hours
+minutes = int(_time[14:-10]) #only retains minutes
+
+#if minutes >= 30: #quits if minutes > 30
+#    quit()
 
 _time = _time[:-7] #simplifies time expression
 
@@ -93,82 +99,132 @@ gbpusd = 'GBPUSD=X'
 nzdjpy = 'NZDJPY=X'
 nzdusd = 'NZDUSD=X'
 nzdcad = 'NZDCAD=X'
+nzdchf = 'NZDCHF=X'
 usdcad = 'CAD=X'
 usdchf = 'CHF=X'
 usdjpy = 'JPY=X'
+
+forex_list = [  eurusd,eurjpy,gbpusd,eurgbp,cadjpy,usdcad,eurnzd,audcad,audjpy,nzdjpy,audusd,usdjpy,\
+                usdchf,nzdusd,euraud,nzdcad,cadchf,chfjpy,audnzd,audchf,eurchf,gbpcad,gbpjpy,eurcad]
+
 
 #Crypto and yf codes
 btc = 'BTC-USD'
 eth = 'ETH-USD'
 
-#analyses only crypto on weekends
-if datetime.today().weekday() >= 5: 
-    trade_list = [btc,eth]
-#Trade List
-else:
-    trade_list = [  eurusd,eurjpy,gbpusd,eurgbp,cadjpy,usdcad,eurnzd,audcad,audjpy,nzdjpy,audusd,usdjpy,eurcad,\
-                    usdchf,nzdusd,euraud,nzdcad,cadchf,chfjpy,audnzd,audchf,eurchf,gbpcad,gbpjpy,\
-                    btc,eth]
+crypto_list = [btc,eth]
 
+#EU Stocks and yf codes
+ads = 'ADS.DE' #adidas
+ezj = 'EZJ.L' #easyjet
+arbs = 'AIR.PA' #airbus
+hnk = 'HEIO.AS' #heineken
+rr = 'RR.L' #Rolls Royce
+san = 'SAN' #santander
+
+eu_list = [ads,ezj,arbs,hnk,rr,san]
+
+#US Stocks and yf codes
+tsla = 'TSLA' #tesla
+amzn = 'AMZN' #amazon
+fb = 'FB' #facebook
+amd = 'AMD' #AMD
+
+us_list = [tsla,amzn,fb,amd]
+
+#ETFs and yf codes
+sp500 = 'SPY' #S&P500
+dow = 'DIA' #Dow Jones
+
+etf_list = [sp500,dow]
+
+trade_list = []
+
+#all time determination done in +8 timezone
+if datetime.today().weekday() >= 5: 
+    trade_list = crypto_list #analyses only crypto on weekends
+else:
+    trade_list = trade_list + forex_list + crypto_list
+
+'''
+    #need to finalise trade conditions
+    if hours >= 15:
+        trade_list = trade_list + eu_list
+    if  hours >= 22 or hours <= 4:
+        trade_list = trade_list + us_list + etf_list
+'''
 n=0 #Trade signals detected
 p=0 #Trade items analysed
 
 for i in trade_list:
     #download data
+    print(i)
     data = yf.download(i,period='1mo',interval='1h')
 
     #100MA
     data['100MA'] = data['Close'].rolling(window=100).mean()
 
     #Stochastic oscillator
-    # Define periods
     k_period = 15
     d_period = 3
-    # Adds a "n_high" column with max value of previous 14 periods
-    n_high = data['High'].rolling(k_period).max()
-    # Adds an "n_low" column with min value of previous 14 periods
-    n_low = data['Low'].rolling(k_period).min()
-    # Uses the min/max values to calculate the %k (as a percentage)
-    data['%K'] = (data['Close'] - n_low) * 100 / (n_high - n_low)
-    # Uses the %k to calculates a SMA over the past 3 values of %k
+    
+    data['n_high'] = data['High'].rolling(k_period).max()
+    data['n_low'] = data['Low'].rolling(k_period).min()
+    data['%K'] = (data['Close'] - data['n_low']) * 100 / (data['n_high'] -data['n_low'])
     data['%D'] = data['%K'].rolling(d_period).mean()
 
     #RSI
     data['RSI'] = pta.rsi(data['Close'], length = 14)
 
     #MACD
-    # Get the 21-period EMA of the closing price
-    slow = data['Close'].ewm(span=21, adjust=False, min_periods=21).mean()
-    # Get the 8-period EMA of the closing price
-    fast = data['Close'].ewm(span=8, adjust=False, min_periods=8).mean()
-    # Subtract the 21-period EMA from the 8-period EMA to get the MACD
+    slow_p = 21
+    fast_p = 8
+    sig_p = 5
+
+    slow = data['Close'].ewm(span=slow_p, adjust=False, min_periods=slow_p).mean()
+    fast = data['Close'].ewm(span=fast_p, adjust=False, min_periods=fast_p).mean()
     data['MACD'] = fast - slow
-    # Get the 5-period EMA of the MACD for the Signal line
-    data['MACD_S'] = data['MACD'].ewm(span=5, adjust=False, min_periods=5).mean()
-    #get difference between MACD and Signal line
+    data['MACD_S'] = data['MACD'].ewm(span=sig_p, adjust=False, min_periods=sig_p).mean()
     data['MACD_dif'] = abs(data['MACD']-data['MACD_S'])
 
     #ATR
+    atr_p = 14 #ATR period
+
     high_low = data['High'] - data['Low']
     high_close = np.abs(data['High'] - data['Close'].shift())
     low_close = np.abs(data['Low'] - data['Close'].shift())
     ranges = pd.concat([high_low, high_close, low_close], axis=1)
     true_range = np.max(ranges, axis=1)
-    data['ATR'] = true_range.rolling(14).sum()/14
+    data['ATR'] = true_range.rolling(atr_p).sum()/atr_p
 
-    last_7 = data.tail(8) #gets last 7 confirmed rows
-    _2nd_last = data.iloc[-3]
+    last_10 = data.iloc[-12:-2] #gets last 10 confirmed rows
     last = data.iloc[-2] #last confirmed candle
+
+    '''
+    #Graphing
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=data.index, y= data['%K'],line=dict(color='blue', width=.7), name = '%K'))
+    fig.add_trace(go.Scatter(x=data.index, y= data['%D'],line=dict(color='red', width=1.5), name = '%D'))
+    fig.add_trace(go.Scatter(x=data.index, y= data['MACD'],line=dict(color='lightblue', width=1.5), name = 'MACD'))
+    fig.add_trace(go.Scatter(x=data.index, y= data['MACD_S'],line=dict(color='lightgreen', width=1.5), name = 'Signal'))
+    fig.add_trace(go.Scatter(x=data.index, y= data['ATR'],line=dict(color='orange', width=1.5), name = 'ATR'))
+
+    fig.update_layout(title = i)
+    fig.show()
+    '''
 
     #Stop loss calculation (%)
     if i == eurjpy or i == cadjpy or i == audjpy or i == nzdjpy or i == chfjpy or i == gbpjpy or i == usdjpy: #all JPY pairs
         stop_loss = last['ATR'] * 100
     elif i == btc:
-        stop_loss = 10
+        stop_loss = last['ATR'] / 100 * 2
     elif i == eth:
-        stop_loss = 20
-    else:
+        stop_loss = last['ATR'] / 10 * 2
+    else: #works with pairs with x50 leverage
         stop_loss = last['ATR'] * 10000
+        if i == nzdchf: #for pairs with x100 leverage
+            stop_loss = stop_loss * 2
 
     #Position Size
     abs_risk = cap * risk #Absolute risk
@@ -180,54 +236,49 @@ for i in trade_list:
     - or just double TP
     '''
 
-    #Buy signal no TP
-    if last['%K'] > last['%D'] and min(last_7['%K']) < 20 and \
-        last['RSI'] >= 50 and _2nd_last['RSI'] <= 50 and min(last_7['RSI']) < 30 and \
-        (last['MACD'] > last['MACD_S'] or _2nd_last['MACD_dif'] > last['MACD_dif']) and last['MACD'] < 0 and\
-        min(last_7['100MA']) < last['100MA']:
-        gmail('Buy signal for '+i,\
-            '\nStop Loss (%) to be set at '+str(stop_loss)+ \
-            '\nPosition size: '+str(pos)+ \
-            '\n\nSent at '+_time+'.')
+    #Buy signal
+    if last['%K'] >= last['%D'] and min(last_10['%K']) <= 20\
+        and last['MACD'] >= last['MACD_S'] and last['MACD'] <= 0\
+        and last['RSI'] >= 50 and last['RSI'] <= 60 and min(last_10['RSI']) <= 40:
+        subject = 'Buy signal for '+i
+        if last['100MA'] > min(last_10['100MA']):
+            body =  'SL(%) to be set at '+str(stop_loss)+\
+                    '\nTP(%) to be set at '+str(stop_loss*2)+\
+                    '\nPosition size: '+str(pos)+ \
+                    '\n\nSent at '+_time+'.'
+        else:
+            body =  'SL(%) to be set at '+str(stop_loss)+\
+                    '\nPosition size: '+str(pos)+\
+                    '\n\nSent at '+_time+'.'
+        print('Buy signal detected for '+i)
+        gmail(subject,body)
         n=n+1
         
-    #Sell signal no TP
-    elif last['%K'] < last['%D'] and max(last_7['%K']) > 80 and \
-        last['RSI'] <= 50 and  _2nd_last['RSI'] >= 50 and max(last_7['RSI']) > 70 and \
-        (last['MACD'] < last['MACD_S'] or _2nd_last['MACD_dif'] > last['MACD_dif']) and last['MACD'] > 0 and\
-        max(last_7['100MA']) > last['100MA']:
-        gmail('Sell signal for '+i,\
-            '\nStop Loss (%) to be set at '+str(stop_loss)+ \
-            '\nPosition size: '+str(pos)+\
-            '\n\nSent at '+_time+'.')
-        n=n+1
-
-    #Buy signal with TP
-    elif last['%K'] > last['%D'] and min(last_7['%K']) < 20 and \
-        last['RSI'] >= 50 and _2nd_last['RSI'] <= 50 and min(last_7['RSI']) < 30 and \
-        (last['MACD'] > last['MACD_S'] or _2nd_last['MACD_dif'] > last['MACD_dif']) and last['MACD'] < 0:
-        gmail('Buy signal for '+i,\
-            '\nStop Loss (%) to be set at '+str(stop_loss)+ \
-            '\nTake Profit (%) to be set at '+str(stop_loss * 2)+ \
-            '\nPosition size: '+str(pos)+ \
-            '\n\nSent at '+_time+'.')
-        n=n+1
-        
-    #Sell signal with TP
-    elif last['%K'] < last['%D'] and max(last_7['%K']) > 80 and \
-        last['RSI'] <= 50 and  _2nd_last['RSI'] >= 50 and max(last_7['RSI']) > 70 and \
-        (last['MACD'] < last['MACD_S'] or _2nd_last['MACD_dif'] > last['MACD_dif']) and last['MACD'] > 0:
-        gmail('Sell signal for '+i,\
-            '\nStop Loss (%) to be set at '+str(stop_loss)+ \
-            '\nTake Profit (%) to be set at '+str(stop_loss * 2)+ \
-            '\nPosition size: '+str(pos)+\
-            '\n\nSent at '+_time+'.')
+    #Sell signal
+    elif last['%K'] <= last['%D'] and max(last_10['%K']) >= 80\
+        and last['MACD'] <= last['MACD_S'] and last['MACD'] >= 0\
+        and last['RSI'] <= 50 and last['RSI'] >= 40  and max(last_10['RSI']) >= 60:
+        subject = 'Sell signal for '+i
+        if last['100MA'] < min(last_10['100MA']):
+            body =  'SL(%) to be set at '+str(stop_loss)+\
+                    '\nTP(%) to be set at '+str(stop_loss*2)+\
+                    '\nPosition size: '+str(pos)+\
+                    '\n\nSent at '+_time+'.'
+        else:
+            body =  'SL(%) to be set at '+str(stop_loss)+\
+                    '\nPosition size: '+str(pos)+\
+                    '\n\nSent at '+_time+'.'
+        print('Sell signal detected for '+i)
+        gmail(subject,body)
         n=n+1
     else:
         print('No signals detected for '+i)
+    
     p=p+1
+
 print('Securities analysed: '+str(p))
 print('Signals detected: '+str(n))
+
 #Summary email for sanity checks
 #gmail('Market analyser ran at '+_time,str(p)+' trade items were analysed.\n' +str(n)+' trading signals were detected.')
 
